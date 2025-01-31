32551b0de7e90eb3975c3bc92f111f47960a3fd4
RLUCAS <rebouletlucas@gmail.com>
2025-01-31 16:28:35 +0100

    add page details for user

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              "Mon App",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "Mon App",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           ),
-          backgroundColor: Colors.blue,
         ),
-        body: const ContentList(),
+        backgroundColor: Colors.blue,
       ),
+      body: const ContentList(),
     );
   }
 }
@@ -36,7 +45,7 @@ class ContentList extends StatelessWidget {
           (index) => {
         "title": "Nom ${index + 1}",
         "description": "Description ${index + 1}.",
-        "imageUrl": "https://picsum.photos/200/300${index + 1}"
+        "imageUrl": "https://picsum.photos/200/300?random=${index + 1}"
       },
     );
 
@@ -44,29 +53,89 @@ class ContentList extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: ListTile(
-            leading: CircleAvatar(
-              radius: 25,
-              backgroundImage: NetworkImage(items[index]["imageUrl"]!),
-            ),
-            title: Text(
-              items[index]["title"]!,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailScreen(
+                  title: items[index]["title"]!,
+                  description: items[index]["description"]!,
+                  imageUrl: items[index]["imageUrl"]!,
+                ),
+              ),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage(items[index]["imageUrl"]!),
+              ),
+              title: Text(
+                items[index]["title"]!,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                items[index]["description"]!,
+                style: const TextStyle(fontSize: 14, color: Colors.grey),
               ),
             ),
-            subtitle: Text(
-              items[index]["description"]!,
-              style: const TextStyle(fontSize: 14, color: Colors.grey),
-            ),
-            onTap: () {
-            },
           ),
         );
       },
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  const DetailScreen({super.key,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Détails"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(imageUrl),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
