e95b4f60db3dc2158c6e2a5943a777363d164dfc
RLUCAS <rebouletlucas@gmail.com>
2025-03-04 23:32:21 +0100

    add modal update user data in page profile

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,33 +1,40 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-final SupabaseClient supabase = Supabase.instance.client;
-
 class UserService {
-  // Fonction pour récupérer les informations de l'utilisateur connecté
-  static Future<Map<String, dynamic>?> getUserData() async {
-    try {
-      final userId = supabase.auth.currentUser?.id;
-      if (userId != null) {
-        // Récupérer les données de l'utilisateur à partir de la table `users`
-        final response = await supabase
-            .from('users')
-            .select()
-            .eq('id', userId)
-            .maybeSingle();  // On utilise maybeSingle() pour récupérer un seul résultat
+  static final SupabaseClient supabase = Supabase.instance.client;
 
-        if (response != null) {
-          return response;  // Retourner les données de l'utilisateur
-        } else {
-          print("Aucun utilisateur trouvé avec cet ID");
-          return null;
-        }
-      } else {
-        print("Utilisateur non connecté");
-        return null;
-      }
-    } catch (e) {
-      print("Erreur lors de la récupération des données de l'utilisateur : $e");
-      return null;
+  // Récupérer les données de l'utilisateur
+  static Future<Map<String, dynamic>?> getUserData() async {
+    final user = supabase.auth.currentUser;
+    if (user == null) return null;
+
+    final response = await supabase
+        .from('users')
+        .select()
+        .eq('id', user.id)
+        .single();
+
+    print("Données récupérées : $response"); // Debug
+
+    return response;
+  }
+
+  // Mettre à jour les données de l'utilisateur
+  static Future<void> updateUserData(Map<String, dynamic> updatedData) async {
+    final user = supabase.auth.currentUser;
+    if (user == null) {
+      print("Aucun utilisateur connecté.");
+      return;
     }
+
+    print("Données envoyées : $updatedData"); // Debug
+
+    final response = await supabase
+        .from('users')
+        .update(updatedData)
+        .eq('id', user.id)
+        .select();
+
+    print("Réponse de Supabase : $response"); // Debug
   }
 }
